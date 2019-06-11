8d3dea696e206d92e5c0928c1eb2e3a6061429a8
sandalu95 <sandalukalpanee@gmail.com>
2019-06-11 20:27:28 +0530

    Style To-do item

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // Set dynamic classes
+  setClasses() {
+    const classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    };
+    return classes;
+  }
+
+}
