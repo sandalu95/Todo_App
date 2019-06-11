01f79f58ff4e0f3e8744fc5ca22151dc1b306226
sandalu95 <sandalukalpanee@gmail.com>
2019-06-11 19:33:06 +0530

    Add Todos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = 'Ishara';
+  constructor() {
+    this.changeName('Sandalu Kalpanee');
+  }
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
